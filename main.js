a5242b060cd97c60bb1236f2d20f83b7f1d9da0e
Thapelo <Thapelo.Kekana1@fnb.co.za>
2022-07-08 03:47:15 +0200

    scope

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-// Starting Point -- main.js
+
+// ES5
+function checkScope() {
+    var i = 'function scope';
+    if (true) {
+      i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+  }
+
+
+// ES6
+  function checkScope() {
+    let  i = 'function scope';
+    if (true) {
+      let i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+  }
