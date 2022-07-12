b7331a6b0b5da01aee8f5957a613a4a4ed53ed43
Thapelo <Thapelo.Kekana1@fnb.co.za>
2022-07-12 04:30:21 +0200

    Object Destructuring

--- main.js
+++ main.js
@@ -1,10 +1,65 @@
-/*
-    Using the spread operator, copy all of the contents of arr1 into another array arr2.
+/* 
+   ES6 Object with Destructuring: 
+   Normal Object
+*/
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+   
+  const today = HIGH_TEMPERATURES.today;
+  const tomorrow = HIGH_TEMPERATURES.tomorrow;
+
+
+/* 
+   ES6 Object with Destructuring: 
+   Use Destructuring Assignment to Extract Values from Objects
+*/
+  const HIGH_TEMPERATURESDestructured = {
+    todayDestructured: 77,
+    tomorrowDestructured: 80
+  };
+
+  
+  const {todayDestructured} = HIGH_TEMPERATURESDestructured;
+  const {tomorrowDestructured} = HIGH_TEMPERATURESDestructured;
+
+
+
+/* 
+   ES6 Object with Destructuring: 
+  Use Destructuring Assignment to Assign Variables from Objects
 */
 
-const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
-let arr2;
+const HIGH_TEMPERATURESDestructuredp2 = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+    
+  const {today: highTodayp1} = HIGH_TEMPERATURESDestructuredp2;
+  const {tomorrow: highTomorrow} = HIGH_TEMPERATURESDestructuredp2; 
 
-arr2 = [...arr1]; 
 
-console.log(arr2);
+  
+/* 
+   ES6 Object with Destructuring: 
+  Use Destructuring Assignment to Assign Variables from Nested Objects
+
+*/
+
+const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+  };
+
+
+    
+  const { today: { low:lowToday } } = LOCAL_FORECAST;
+  const { today: { high: highToday } } = LOCAL_FORECAST;
+
+  console.log(highToday);
+
+
