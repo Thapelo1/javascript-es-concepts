b0ebda3e78dfccb2d334974eb34cd4c30773f566
Thapelo <Thapelo.Kekana1@fnb.co.za>
2022-07-12 05:01:49 +0200

    Array  Destructuring

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 ES6, released in 2015, added many powerful new features to the language. In this repo, I cover these new features, including arrow functions, destructuring, classes, promises, and modules.
 
 
-# Object Destructuring
+# Array Destructuring
 Destructuring assignment is a new syntax introduced in ES6 that allows you to neatly assign values taken directly from an object.
 
--- main.js
+++ main.js
@@ -1,65 +1,41 @@
 /* 
-   ES6 Object with Destructuring: 
-   Normal Object
+   ES6 makes destructuring arrays as easy as destructuring objects.
 */
-const HIGH_TEMPERATURES = {
-    yesterday: 75,
-    today: 77,
-    tomorrow: 80
-  };
-   
-  const today = HIGH_TEMPERATURES.today;
-  const tomorrow = HIGH_TEMPERATURES.tomorrow;
+
+const [a, b] = [1, 2, 3, 4, 5, 6];
+console.log(`The value of a: ${a} and the value of b: ${b}` )
+
+/* 
+   Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements
+*/
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+
+function removeFirstTwo(list) {
+  const [a, b, ...arr] = list;
+  return arr;
+}
+
+const arr = removeFirstTwo(source);
 
 
 /* 
-   ES6 Object with Destructuring: 
-   Use Destructuring Assignment to Extract Values from Objects
+   Use Destructuring Assignment to Pass an Object as a Function's Parameters
 */
-  const HIGH_TEMPERATURESDestructured = {
-    todayDestructured: 77,
-    tomorrowDestructured: 80
-  };
 
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+  };
   
-  const {todayDestructured} = HIGH_TEMPERATURESDestructured;
-  const {tomorrowDestructured} = HIGH_TEMPERATURESDestructured;
 
+  const half = ({max, min}) => (max + min) / 2.0; 
 
+  console.log(half);
 
-/* 
-   ES6 Object with Destructuring: 
-  Use Destructuring Assignment to Assign Variables from Objects
-*/
-
-const HIGH_TEMPERATURESDestructuredp2 = {
-    yesterday: 75,
-    today: 77,
-    tomorrow: 80
-  };
-    
-  const {today: highTodayp1} = HIGH_TEMPERATURESDestructuredp2;
-  const {tomorrow: highTomorrow} = HIGH_TEMPERATURESDestructuredp2; 
-
-
-  
-/* 
-   ES6 Object with Destructuring: 
-  Use Destructuring Assignment to Assign Variables from Nested Objects
-
-*/
-
-const LOCAL_FORECAST = {
-    yesterday: { low: 61, high: 75 },
-    today: { low: 64, high: 77 },
-    tomorrow: { low: 68, high: 80 }
-  };
-
-
-    
-  const { today: { low:lowToday } } = LOCAL_FORECAST;
-  const { today: { high: highToday } } = LOCAL_FORECAST;
-
-  console.log(highToday);
 
 
